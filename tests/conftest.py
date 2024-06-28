79eceb99e5cc1493dd787653fcc788545a9ead8f
Daim <daimabbask05@gmail.com>
2024-06-28 12:19:24 +0500

    Setting up the right directories

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,36 +3,130 @@ from pytest_mysql import factories
 from contextlib import contextmanager
 from neo4j import GraphDatabase
 from pytest_mysql.executor_noop import NoopMySQLExecutor
+from traits.implementation import TraitsUtility
 
+# Default configurations
+from traits.interface import BASE_USER_NAME, BASE_USER_PASS, ADMIN_USER_NAME, ADMIN_USER_PASS
+
+################################################################################
+# MariaDB fixtures
+################################################################################
 
 @pytest.fixture(scope="session")
 def mariadb_host(request):
+    """
+    Return the HOST parameter to connect to a MariaDB. By default returns localhost's IP
+    """
     return request.config.getoption("--mysql-host") if request.config.getoption("--mysql-host") is not None else "127.0.0.1"
 
 @pytest.fixture(scope="session")
 def mariadb_port(request):
+    """
+    Return the PORT parameter to connect to a MariaDB. By default returns 3306
+    """
     return request.config.getoption("--mysql-port") if request.config.getoption("--mysql-port") is not None else 3306
 
-# This factory returns instances of connections to mariadb/mysql servers
-# mysql_in_docker = factories.mysql_noproc(host=mariadb_host, user="root")
 
 @pytest.fixture(scope="session")
-def mariadb_in_docker(mariadb_host, mariadb_port):
-	mysql_executor = NoopMySQLExecutor(
+def root_mariadb_in_docker(mariadb_host, mariadb_port):
+    """
+    Return a Dummy executor needed to spin off the database using pytest_mysql import factories
+    """
+    mysql_executor = NoopMySQLExecutor(
             user="root",
             host=mariadb_host,
             port=int(mariadb_port),
         )
-    
-	with mysql_executor:
-         yield mysql_executor
+
+    with mysql_executor:
+            yield mysql_executor
 
 # This creates also a db named test... If I am not mistaked, this is deleted after each test execution
-mysql = factories.mysql("mariadb_in_docker", passwd="mypass")
+root_connection = factories.mysql("root_mariadb_in_docker", passwd="root-pass")
+
+@pytest.fixture
+def mariadb_database():
+    """
+    Return the name of the test database
+    """
+    return "test"
 
 
+@pytest.fixture
+def mariadb(root_connection):
+    """
+    This fixture creates a Maria DB called "test" and initializes it with the YOUR code inside the
+    TraitsUtility.generate_sql_initialization_code. This code should create the tables and the users (traits and admin)
+    with the right permissions
+    """
+
+    cur = root_connection.cursor()
+    cur.execute("BEGIN;")
+    for sql_statement in TraitsUtility.generate_sql_initialization_code():
+        cur.execute(sql_statement)
+    cur.execute("COMMIT;")
+
+    yield root_connection
+
+
+@pytest.fixture
+def connection_factory(mariadb, mariadb_host, mariadb_port, mariadb_database):
+    """ 
+    This code retuns an object that can create connections to a database given user and password
+    """
+    @contextmanager
+    def _gen_connection(user, password):
+        """ Generate a connection to the database """
+        import mysql.connector
+        from mysql.connector import Error
+
+        assert user != "root", "Do not create connections to the db using Root!"
+
+        # Try to create the connection, if succeeded eventually close it
+        connection = mysql.connector.connect(host=mariadb_host,
+                                             database=mariadb_database,
+                                             user=user,
+                                             port=mariadb_port,
+                                             password=password)
+        try:
+            if connection.is_connected():
+                db_Info = connection.get_server_info()
+                # print("Connected to MariaDB Server version ", db_Info)
+                cursor = connection.cursor()
+                cursor.execute('SET TRANSACTION ISOLATION LEVEL READ COMMITTED;')
+                cursor.execute("select database();")
+                record = cursor.fetchone()
+                # print("You're connected to database: ", record)
+
+                yield connection
+
+        except Error as e:
+            print("Error while connecting to MySQL", e)
+        finally:
+            if connection.is_connected():
+                cursor.close()
+                connection.close()
+
+    yield _gen_connection
+
+
+@pytest.fixture
+def rdbms_connection(connection_factory):
+    with connection_factory(BASE_USER_NAME, BASE_USER_PASS) as connection:
+       yield connection
+
+@pytest.fixture
+def rdbms_admin_connection(connection_factory):
+    with connection_factory(ADMIN_USER_NAME, ADMIN_USER_PASS) as connection:
+       yield connection
+       
+################################################################################
+# Neo4J fixtures
+################################################################################
 def pytest_addoption(parser):
-    """ Register the additional options to make Neo4j Fixtures work also on GitHub Actions """
+    """ 
+    Register the additional options to make Neo4j Fixtures work also on GitHub Actions
+    """
     try:
         parser.addoption(
             '--neo4j-web-port', action='store', default="", help='Web Port to connect to Neo4j'
@@ -47,10 +141,6 @@ def pytest_addoption(parser):
         pass
 
 
-@pytest.fixture
-def mariadb_database():
-    return "test"
-
 
 @pytest.fixture
 def neo4j_db_port(request):
@@ -60,62 +150,6 @@ def neo4j_db_port(request):
 @pytest.fixture
 def neo4j_db_host(request):
 	return request.config.getoption("--neo4j-host")
-
-
-@pytest.fixture
-def mariadb(mysql):
-    """
-    This fixture creates a db called test initialized with the YOUR code inside the generate_sql_initialization_code
-    method
-    :param mysql:
-    :return:
-    """
-    from solution.database import generate_sql_initialization_code
-
-    cur = mysql.cursor()
-    cur.execute("BEGIN;")
-    for sql_statement in generate_sql_initialization_code():
-        cur.execute(sql_statement)
-    cur.execute("COMMIT;")
-
-    yield mysql
-
-
-@pytest.fixture
-def connection_factory(mariadb_host, mariadb_port, mariadb):
-    """ This code retuns an ojbect that can create connections to a database """
-
-    @contextmanager
-    def _gen_connection():
-        """ Generate a connection to the database """
-        import mysql.connector
-        from mysql.connector import Error
-        # Try to create the connection, if succeeded eventually close it
-        connection = mysql.connector.connect(host=mariadb_host,
-                                             database="test",
-                                             user="root",
-                                             port=mariadb_port,
-                                             password="mypass")
-        try:
-            if connection.is_connected():
-                db_Info = connection.get_server_info()
-                # print("Connected to MariaDB Server version ", db_Info)
-                cursor = connection.cursor()
-                cursor.execute("select database();")
-                record = cursor.fetchone()
-                print("You're connected to database: ", record)
-
-                yield connection
-
-        except Error as e:
-            print("Error while connecting to MySQL", e)
-        finally:
-            if connection.is_connected():
-                cursor.close()
-                connection.close()
-                # print("MySQL connection is closed")
-
-    yield _gen_connection
 
 
 @pytest.fixture
